4b8d107422ba98aab0f99de2ed3f801b771e8172
nipun.walia2018@gmail.com <nipun.walia2018@gmail.com>
2024-05-21 19:17:22 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,87 @@
+// import Navbar from "./../components/Layouts/Navbar";
+// import MainBanner from "../components/HomePages/DefaultHome/MainBanner";
+// import Services from "../components/HomePages/DefaultHome/Services";
+// import About from "../components/HomePages/DefaultHome/About";
+// import OurWorks from "../components/HomePages/DefaultHome/OurWorks";
+// import WorkProcess from "../components/HomePages/DefaultHome/WorkProcess";
+// import Feedback from "../components/Common/Feedback";
+// import Team from "../components/Common/Team";
+// import FunFacts from "../components/Common/FunFacts";
+// import LatestNewsSlider from "../components/Common/LatestNewsSlider";
+// import CtaArea from "../components/Common/CtaArea";
+// import Partner from "../components/Common/Partner";
+// import Footer from "../components/Layouts/Footer";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Navbar />
+
+//       <MainBanner />
+
+//       <Services />
+
+//       <About />
+
+//       <OurWorks />
+
+//       <WorkProcess />
+
+//       <Feedback />
+
+//       <Team />
+
+//       <FunFacts />
+
+//       <LatestNewsSlider />
+
+//       <CtaArea />
+
+//       <Partner />
+
+//       <Footer />
+//     </>
+//   );
+// }
+import React from "react";
+import NavbarTwo from "../components/Layouts/NavbarTwo";
+import MainBanner from "../components/HomePages/StudioAgency/MainBanner";
+import Services from "../components/HomePages/StudioAgency/Services";
+import OurWorks from "../components/HomePages/StudioAgency/OurWorks";
+import FunFactsTwo from "../components/Common/FunFactsTwo";
+import About from "../components/HomePages/StudioAgency/About";
+import Feedback from "../components/Common/Feedback";
+import LatestNewsSlider from "../components/Common/LatestNewsSlider";
+import CtaAreaTwo from "../components/Common/CtaAreaTwo";
+import Partner from "../components/Common/Partner";
+import Footer from "../components/Layouts/Footer";
+
+export default function Page() {
+  return (
+    <>
+      <NavbarTwo />
+
+      <MainBanner />
+
+      <Services />
+
+      <About />
+
+      <Partner />
+
+      <OurWorks />
+
+      <FunFactsTwo />
+
+      <Feedback />
+
+      <LatestNewsSlider />
+
+      <CtaAreaTwo />
+
+      
+
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/HomePages/ItAgencyTwo/OurServices.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+const servicesData = [
+  {
+    iconName: "pe-7s-comment bg-13c4a1",
+    title: "Finance",
+    shortText:
+      "Enhance regulatory compliance, streamline budgeting, and optimize workforce management.",
+    viewDetails: "/services/service-details/",
+    aosDelay: "100",
+  },
+  {
+    iconName: "pe-7s-display2 bg-6610f2",
+    title: "Recruitment",
+    shortText:
+      "Automate resume screening, scheduling, and onboarding for efficient talent acquisition.",
+    viewDetails: "/services/service-details/",
+    aosDelay: "200",
+  },
+  {
+    iconName: "pe-7s-light bg-ffb700",
+    title: "Logistics",
+    shortText:
+      "Improve fleet management and warehouse operations to boost efficiency and reduce costs.",
+    viewDetails: "/services/service-details/",
+    aosDelay: "300",
+  },
+  {
+    iconName: "pe-7s-phone bg-fc3549",
+    title: "Healthcare",
+    shortText:
+      "Transform patient monitoring, appointment scheduling, and EHR management for better care.",
+    viewDetails: "/services/service-details/",
+    aosDelay: "400",
+  },
+  {
+    iconName: "pe-7s-cart bg-00d280",
+    title: "Manufacturing",
+    shortText:
+      "Optimize inventory, enhance maintenance practices, and streamline workflow automation.",
+    viewDetails: "/services/service-details/",
+    aosDelay: "500",
+  },
+  {
+    iconName: "pe-7s-users bg-ff612f",
+    title: "Retail",
+    shortText:
+      "Automate order fulfillment and inventory management to improve efficiency and customer satisfaction.",
+    viewDetails: "/services/service-details/",
+    aosDelay: "600",
+  },
+];
+
+const OurServices: React.FC = () => {
+  return (
+    <>
+      <div className="pt-100 pb-70">
+        <div className="container">
+          <div className="section-title">
+            <h2>Industries Benefiting from Automation</h2>
+            {/* <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna.
+            </p> */}
+          </div>
+
+          <div className="row justify-content-center">
+            {servicesData &&
+              servicesData.slice(0, 6).map((value, i) => (
+                <div 
+                  className="col-lg-4 
+                  col-sm-6" 
+                  key={i}
+                  data-aos="fade-in"
+                  data-aos-duration="1000"
+                  data-aos-delay={value.aosDelay}
+                >
+                  <div className="service-card-one bg-fcfbfb text-center">
+                    <i className={value.iconName}></i>
+                    <h3>
+                      <Link href={value.viewDetails}>{value.title}</Link>
+                    </h3>
+                    <p>{value.shortText}</p>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OurServices;
