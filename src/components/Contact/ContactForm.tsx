4130a3cda464abeb24c0c562a0c630be13cd0d4f
unknown <singhraman3@hotmail.com>
2024-11-07 16:09:54 +0530

    fix - Emailjs Template changes

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -132,6 +132,8 @@ const ContactForm: React.FC = () => {
     const templateParams = {
       to_name: 'Dhruv',
       from_name: data.name,
+      phone: data.mobile,
+      email: data.email,
       message: data.message,
     };
     try {
